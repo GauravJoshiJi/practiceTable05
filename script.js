d209a66da61db730def70b679c1c9d60ec012a2d
GauravJoshiJi <gauravjoshi1997.joshi@gmail.com>
2023-12-28 15:24:20 +0530

    API data into table

--- script.js
+++ script.js
@@ -10,14 +10,33 @@ fetch("https://dummyjson.com/products")
 
     let product = "";
     productData.map((value) => {
+      let rating = Math.round(value.rating);
+      let newRating;
+      switch (rating) {
+        case 1:
+          newRating = "⭐";
+          break;
+        case 2:
+          newRating = "⭐⭐";
+          break;
+        case 3:
+          newRating = "⭐⭐⭐";
+          break;
+        case 4:
+          newRating = "⭐⭐⭐⭐";
+          break;
+        default:
+          newRating = "⭐⭐⭐⭐⭐";
+      }
+
       product += `
       <tr>
-      <td>No</td>
-      <td>titl</td>
-      <td>des</td>
-      <td>pri</td>
-      <td>rati</td>
-      <td>img</td>
+      <td>${value.id}</td>
+      <td>${value.title}</td>
+      <td>${value.description}</td>
+      <td>${value.price}</td>
+      <td>${newRating}</td>
+      <td><img src="${value.thumbnail}" /></td>
     </tr>`;
     });
     document.getElementById("table_body").innerHTML = product;
